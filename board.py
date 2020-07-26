4c2b1f8ba61a2cd81b3904d74edf21b69bf967da
froggomad <kenny.dubroff@kennydubroff.com>
2020-07-25 21:11:40 -0700

    updated board and game to allow for adjustable window height

--- board.py
+++ board.py
@@ -1,19 +1,22 @@
 import pygame
+class Size:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 class Board:
     """A square grid of `num_cells` size"""
-    def __init__(self, generation, window_width, num_cells=25):
+    def __init__(self, generation, window_width, window_height, num_cells=25):        
         self._generation = generation
         self._is_user_interaction_enabled = True
         self._num_cells = num_cells
-        self._cell_size = window_width//num_cells
-        self._size = window_width
+        self._size = Size(window_width, window_height)
     
     def size(self):
         return self._size
 
     def cell_size(self):
-        return self._cell_size
+        return Size(self._size.width//self._num_cells, self._size.height//self._num_cells)
 
     def get_generation(self):
         return self._generation
@@ -25,25 +28,24 @@ class Board:
         return self._is_user_interaction_enabled
 
     def set_board_size(self, num_cells):
+        # TODO
         """Set the size of the board in a square (ie 25 is 25x25)"""
-        self._num_cells = num_cells
-        self._cell_size = num_cells
     
     def draw_grid(self):
         """Create the grid using the current size and generation"""
         # TODO: Improve performance (do we need a nested for loop?)
         # TODO: Draw generation label
-        from game import WINDOW_HEIGHT
-        from game import WINDOW_WIDTH
         from game import SCREEN
         from game import BLACK
         self._is_user_interaction_enabled = False
         #makes grids of _size (pixels) such that it fits the window size
-        for x in range(WINDOW_WIDTH//self._num_cells):            
-            for y in range(WINDOW_HEIGHT//self._num_cells):                
-                    rect = pygame.Rect(x*self._cell_size, y*self._cell_size,
-                                    self._cell_size, self._cell_size)
-                    pygame.draw.rect(SCREEN, BLACK, rect, 1)
+        for x in range(self._num_cells):            
+            for y in range(self._num_cells):
+                rect = pygame.Rect(x*self.cell_size().width, y*self.cell_size().height,
+                                self.cell_size().width, self.cell_size().height)
+                #print(x*self.cell_size().width)
+                print(y*self.cell_size().height)
+                pygame.draw.rect(SCREEN, BLACK, rect, 1)
 
     def draw_status_bar(self):
         """draw the generation in the lower right corner"""
--- game.py
+++ game.py
@@ -5,33 +5,33 @@ from automata import Line_Three
 
 pygame.init()
 
-WINDOW_HEIGHT = 800
+WINDOW_HEIGHT = 1000
 WINDOW_WIDTH = 800
-SCREEN = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
+SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 CLOCK = pygame.time.Clock()
 FPS = 5
 BLACK = (0, 0, 0)
 WHITE = (245, 245, 245)
 
-board = Board(0, WINDOW_WIDTH, 25)
+board = Board(0, WINDOW_WIDTH, WINDOW_HEIGHT, 25)
 cell_size = board.cell_size()
 board_size = board.size()
-x = cell_size//2
+x = cell_size.width//2
 line = Line_Three()
 while True:
     # TODO: use dt to control movement speed of objects?
     dt = CLOCK.tick(FPS)
     SCREEN.fill(WHITE)
     board.draw_grid()
-    line.draw(SCREEN, x, cell_size)
+    line.draw(SCREEN, x, cell_size.width)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    if x >= board_size:
-        x = cell_size//2
+    if x >= board_size.width:
+        x = cell_size.width//2
     else:
-        x += cell_size
+        x += cell_size.width
     # double buffers by default
     pygame.display.flip()
 
